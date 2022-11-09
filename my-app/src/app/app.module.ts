6ce2dac87f8f65176778219adc28a65152ea4f2b
0410L <oriol.busquet@gmail.com>
2022-11-09 11:05:41 +0100

    1.4

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContadorComponent } from './contador/contador.component';
+import { CarritoComponent } from './carrito/carrito.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContadorComponent
+    ContadorComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/carrito/carrito.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carrito',
+  styleUrls: ['./carrito.component.css'],
+
+  template: `
+  <p></p>
+  <h1>Carrito</h1>
+  <h5>La base es:<strong>{{base}}</strong></h5>
+  <button (click)="acumular(base)">+5</button>
+  <span>{{numero}}</span>
+  <button (click)="acumular(-base)">-5</button>
+  <p></p>
+  <h5>Resultado total:<strong>{{numero}}</strong></h5>
+  <p></p>
+  `
+})
+export class CarritoComponent implements OnInit {
+  numero: number = 0;
+  base: number = 5;
+
+  acumular (valor:number){
+    this.numero+=valor;
+  }
+
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+}
