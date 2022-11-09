ce20cc1ab5e47e83e26d7c8f1875ecc580582913
0410L <oriol.busquet@gmail.com>
2022-11-09 12:52:12 +0100

    1.5

--- my-app/src/app/carrito/carrito.component.ts
+++ my-app/src/app/carrito/carrito.component.ts
@@ -6,20 +6,31 @@ import { Component, OnInit } from '@angular/core';
 
   template: `
   <p></p>
-  <h1>Carrito</h1>
-  <h5>La base es:<strong>{{base}}</strong></h5>
-  <button (click)="acumular(base)">+5</button>
-  <span>{{numero}}</span>
-  <button (click)="acumular(-base)">-5</button>
-  <p></p>
-  <h5>Resultado total:<strong>{{numero}}</strong></h5>
-  <p></p>
+  <div class="container">
+    <h4>¿Que vols fer?</h4>
+    <div class="form-check">
+      <input type="checkbox" (click)="acumular(500)" class= form-check-input>
+      <label class="form-check-label">pagina web (500€)</label>
+    </div>
+    <p></p>
+    <div class="form-check">
+      <input type="checkbox" (click)="acumular(300)" class= form-check-input>
+      <label class="form-check-label">una consultoria SEO (300€)</label>
+    </div>
+    <p></p>
+    <div class="form-check">
+      <input type="checkbox" (click)="acumular(200)" class= form-check-input>
+      <label class="form-check-label">una campanya de Google Ads (200€)</label>
+    </div>
+    <p></p>
+    <p></p>
+    <h5>Preu:<strong>{{numero}}</strong></h5>
+    <p></p>
+  </div>
   `
 })
 export class CarritoComponent implements OnInit {
   numero: number = 0;
-  base: number = 5;
-
   acumular (valor:number){
     this.numero+=valor;
   }
