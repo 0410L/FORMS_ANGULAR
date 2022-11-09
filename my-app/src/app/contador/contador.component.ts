9c57029f75013509b4c745bd67047965058aa29c
0410L <oriol.busquet@gmail.com>
2022-11-09 10:12:49 +0100

    1.3

--- my-app/src/app/contador/contador.component.ts
+++ my-app/src/app/contador/contador.component.ts
@@ -2,12 +2,27 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-contador',
-  templateUrl: './contador.component.html',
-  styleUrls: ['./contador.component.css']
+  styleUrls: ['./contador.component.css'],
+
+  template: `
+  <p></p>
+  <h1>Contador</h1>
+  <h5>La base es:<strong>{{base}}</strong></h5>
+  <button (click)="acumular(base)">+5</button>
+  <span>{{numero}}</span>
+  <button (click)="acumular(-base)">-5</button>
+  <p></p>
+  <h5>Resultado total:<strong>{{numero}}</strong></h5>
+  <p></p>
+  `
 })
 export class ContadorComponent {
   numero: number = 0;
   base: number = 5;
+
+  acumular (valor:number){
+    this.numero+=valor;
+  }
 
   /*
   sumar() {
@@ -18,9 +33,5 @@ export class ContadorComponent {
     this.numero-=1;
   }
   */
-
-  acumular (valor:number){
-    this.numero+=valor;
-  }
 
 }
